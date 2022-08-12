34bc06be8c0dea94526367b8580d97c88b9a994b
ALEXX <alexedgik@gmail.com>
2022-08-12 21:58:44 +0300

    PlantsHandBook2

---
+++ app/src/main/java/com/example/plantshandbook/ContentActivity.kt
@@ -0,0 +1,21 @@
+package com.example.plantshandbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plantshandbook.databinding.ActivityContentBinding
+
+class ContentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityContentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val item = intent.getSerializableExtra("item") as Plant
+        binding.apply {
+            imMain.setImageResource(item. imageId)
+            tvTitle.text = item.title
+            tvContent.text = item.desc
+        }
+    }
+}
